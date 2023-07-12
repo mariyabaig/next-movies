577b46124d054f4bc62164f51882fdbe3d06fc0f
mariyabaig <mariyabaig0106@gmail.com>
2023-07-12 14:13:34 +0530

    style:colors changed

--- src/app/component/Navbar.js
+++ src/app/component/Navbar.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between px-20 py-10 items-center bg-white">
-    <h1 className="text-xl text-gray-800 font-bold">MovieFlix</h1>
+    <nav className="flex justify-between px-20 py-10 items-center bg-slate-900">
+    <h1 className="text-xl text-white font-bold">MovieFlix</h1>
     <div className="flex items-center">
    
       <ul className="flex items-center space-x-6">
-        <li className="font-semibold text-gray-700">
+        <li className="font-semibold text-white">
         <Link href="/">Home</Link>
         
         </li>
-        <li className="font-semibold text-gray-700"><Link href="/about">About</Link></li>
-        <li className="font-semibold text-gray-700">
+        <li className="font-semibold text-white"><Link href="/about">About</Link></li>
+        <li className="font-semibold text-white">
         <Link href="/contact">Contact</Link>
         </li>
       
