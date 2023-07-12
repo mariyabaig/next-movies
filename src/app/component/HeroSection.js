a3e53cd47e3ab6b7c49db489e9afe1ebb1c1ccc3
mariyabaig <mariyabaig0106@gmail.com>
2023-07-12 14:13:44 +0530

    HeroSection added

---
+++ src/app/component/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+   <>
+    <div className='h-screen w-full bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100'>
+        <h1>EXPLORE THESE AMAZING MOVIE REVIEWS</h1>
+    </div>
+   </>
+  )
+}
+
+export default HeroSection
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroSection from './component/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Next app</h1>
+    <main>
+      <HeroSection/>
     </main>
   )
 }
