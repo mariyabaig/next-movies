70f9f5c22a597956baf97bf1f4c3f209e79448fe
mariyabaig <mariyabaig0106@gmail.com>
2023-07-12 13:20:05 +0530

    Routing implemented

--- src/app/component/Navbar.js
+++ src/app/component/Navbar.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-between px-20 py-10 items-center bg-white">
+    <h1 className="text-xl text-gray-800 font-bold">MovieFlix</h1>
+    <div className="flex items-center">
+   
+      <ul className="flex items-center space-x-6">
+        <li className="font-semibold text-gray-700">
+        <Link href="/">Home</Link>
+        
+        </li>
+        <li className="font-semibold text-gray-700"><Link href="/about">About</Link></li>
+        <li className="font-semibold text-gray-700">
+        <Link href="/contact">Contact</Link>
+        </li>
+      
+   
+      </ul>
+    </div>
+  </nav>
   )
 }
 
